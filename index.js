a71c09029b222140a8f0da751098dc1497e15810
Ian Mayton <ian.mayton@gmail.com>
2020-07-08 16:23:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function findMinAndRemoveSorted(array)
+{
+  return array.splice(0, 1)
+}
 
+function merge(firstArray, secondArray)
+{
+  let sortedArray = [];
+  while(firstArray.length > 0 && secondArray.length > 0)
+  {
+    if(firstArray[0] < secondArray[0])
+    {
+      let min = findMinAndRemoveSorted(firstArray)
+      sortedArray.push(min);
+    }
+    else
+    {
+      let min = findMinAndRemoveSorted(secondArray)
+      sortedArray.push(min);
+    }
+  }
+  return sortedArray.concat(firstArray).concat(secondArray);
+}
